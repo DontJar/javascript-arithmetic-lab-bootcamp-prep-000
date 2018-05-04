b9050e4a5383df3db5367a93ed9dbba849bc0d54
Jon Schulman <schulman99@gmail.com>
2018-05-04 21:32:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ function makeInt(a) {
   return parseInt (a, 10)
 }
 
-function isNaN() {
-  return 
+function preserveDecimal(a) {
+  return parseFloat (a)
+}
+
+function isNaN(a) {
+  return parseFloat('a')
 }
