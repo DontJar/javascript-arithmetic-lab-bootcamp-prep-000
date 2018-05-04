f68e10cc60f380b01846ba1f6a1b7c156c858cc4
Jon Schulman <schulman99@gmail.com>
2018-05-04 20:20:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ function add(a, b) {
   return (a + b)
 }
 
-funtion 
+function subtact(a, b) {
+  return (a - b)
+}
+
+function multiply(a, b) {
+  return (a * b)
+}
+
+function divide(a, b) {
+  return (a / b)
+}
